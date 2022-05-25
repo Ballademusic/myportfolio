d4abcb25cdde81a0186e292f7ac52d16f95d4d6f
Ballademusic <96699151+Ballademusic@users.noreply.github.com>
2022-05-25 23:02:45 +0900

    Update ballade.js

--- ballade.js
+++ ballade.js
@@ -1,10 +1,12 @@
 var img = Array("2022.png","flarenewvisual.png","flare.png","mio.png",
 "footprintsofzero.png","sakamata.png","okayu.png","shirona.png","la+.png",
-"CharlotteSuzu.jpg","sakuratomeu.jpg","ironman.png");
+"CharlotteSuzu.jpg","sakuratomeu.jpg","ironman.png","Fubuki.png",
+"flareBirthday.png","tights.jpg","Ayame.jpg","Shion.png","Iroha.png","lower.jpg","colorful.png","HomuraRin.png","Hal.png");
 
 var titles = Array("2022　お正月イラスト","不知火フレア　新ビジュアル",
 "ジト目フレア","ネコミオ","ゼロの足跡","照れ叉","水着おかゆ","シロナさん",
-"ラプラス","CharlotteSuzu","桜兎めう","アイアンマン");
+"ラプラス","CharlotteSuzu","桜兎めう","アイアンマン","水着フブキ","フレア誕生日イラスト",
+"タイツ","お花見お嬢","水着シオン","水着いろは","lowerすいちゃん","colorful","ほむら凛","ハル");
 
 var mediatext0 = "製作期間:3日<br>製作時期:2021年12月末<br>使用ツール:Clip studio Paint<br>服の模様や髪飾りはフリーの素材を使用しました。着物の知識がなかったので、その研究をしながら描きました。";
 var mediatext1 = "製作期間:3日<br>製作時期:2021年12月中旬<br>使用ツール:Clip studio Paint<br>GtC塗りで塗りました。新衣装変更のタイミングでのイラストなので、lack先生の設定画を見ながら、顔面や髪、フェチを出すために脇の書き込みなどに力を入れました。自信作です。";
@@ -18,9 +20,20 @@ var mediatext8 = "製作期間:3日<br>製作時期:2021年11月末<br>使用ツ
 var mediatext9 = "製作期間:3日<br>製作時期:2021年12月末<br>使用ツール:Clip studio Paint<br>2回目のskeb依頼作品。フライパンで料理をするシーンを描きました。髪の情報量を増やそうとして結果雑な印象が出てしまったのは反省点です。";
 var mediatext10 = "製作期間:3日<br>製作時期:2022年1月上旬<br>使用ツール:Clip studio Paint<br>初めてのskeb依頼作品。お正月の衣装をデザインして描いてほしいという内容でした。普段からスコッティ先生のイラストを参考にしているので顔は先生のイラストに寄せることができたと思いますが、塗りや服の固さなど、未熟な点が多く感じました。技術を身に着けてリベンジしたいイラストです。";
 var mediatext11 = "製作期間:3日<br>製作時期:2021年12月末<br>使用ツール:Clip studio Paint<br>大学でインフォグラフィックスの講義があり、イラスト製作時の「レイヤー」についての説明をするために描いたイラスト。金属の光沢を出す点に力を入れました。";
+var mediatext12 = "製作期間:3日<br>製作時期:2022年5月<br>使用ツール:Clip studio Paint,Blender,Photoshop<br>水着企画第3段で描いたイラストです。初めてtwitterで2万いいねをいただきました。前の2作品から反省して、どんな水着か、シチュエーションかなどがわかる構図や、背景の情報量などを増やしたイラストにしてみました。";
+var mediatext13 = "製作期間:3日<br>製作時期:2022年4月上旬<br>使用ツール:Clip studio Paint<br>不知火フレアさんの誕生日記念に描いたイラストです。当日しか時間がなかったので急ぎ足で描いたイラストですが、集中したおかげで描き方の見直しが出来、今の絵柄に固めることにつながった作品になったと思います。";
+var mediatext14 = "製作期間:5日<br>製作時期:2022年3月<br>使用ツール:Clip studio Paint,Blender<br>すこしセンシティブな作品も描いてみようと、足や下着が見える構図を選択しました。blnederもしばらく使っていなかったので、感覚を思い出すためにも背景にblenderを使ってみました。初めてtwitterで1万いいねをいただいた作品です。";
+var mediatext15 = "製作期間:3日<br>製作時期:2022年3月末<br>使用ツール:Clip studio Paint<br>一年前に描いた同じテーマのイラストをリメイクした作品です。このころから自分の中でイラストの描き方がぶれてきてしまい、クオリティが落ちてしまいました。";
+var mediatext16 = "製作期間:3日<br>製作時期:2022年5月<br>使用ツール:Clip studio Paint,Blnder<br>水着企画第2段。前回から反省して色味や線画を変えて描いていますが、色見は画面が赤くなりすぎて全体のイメージとアンマッチしてしまいました。";
+var mediatext17 = "製作期間:3日<br>製作時期:2022年4月末<br>使用ツール:Clip studio Paint<br>水着企画第1段。何かシリーズ物を始めようと思い、twitterのアンケート機能を使ってFFの方が見たいキャラクターの水着イラストを描くことにしました。シリーズにすることによって、そのキャラクターらしさや、そのイラストで見せたいものを意識した描き方ができるようになり、自分の技術の悪いところが反省しやすいのは良い点だと思いました。";
+var mediatext18 = "製作期間:3日<br>製作時期:2022年2月末<br>使用ツール:Clip studio Paint<br>星街すいせいさんのLowerのカバーを聴いて描いたイラストです。雰囲気を大事にしました。";
+var mediatext19 = "製作期間:3日<br>製作時期:2022年4月<br>使用ツール:Clip studio Paint<br>私の誕生日の記念にオリジナルキャラ「ハル」をtwitterアイコン用に描いた作品です。インパクトが出るように、ビビッドな色を背景にしました。";
+var mediatext20 = "製作期間:3日<br>製作時期:2022年4月上旬<br>使用ツール:Clip studio Paint<br>3回目のskeb依頼作品。以前描いたさくらみこさんのイラストの構図で描いてほしいとのご依頼でした。このころから色や質感を意識して描くようになりました。";
+var mediatext21 = "製作期間:3日<br>製作時期:2021年11月上旬<br>使用ツール:Clip studio Paint<br>初のオリジナルキャラクター「ハル」を考案した時のイメージ画として描きました。実はハルは私がモデルのキャラクターなので、私がよく飲むス〇バのコーヒーを持たせてみました。";
 var texts = Array(mediatext0,mediatext1,mediatext2,
     mediatext3,mediatext4,mediatext5,mediatext6,mediatext7,mediatext8,
-    mediatext9,mediatext10,mediatext11);
+    mediatext9,mediatext10,mediatext11,mediatext12,mediatext13,mediatext14,
+    mediatext15,mediatext16,mediatext17,mediatext18,mediatext19,mediatext20,mediatext21);
 
 function changeimg(x){
     var mediaimage = document.getElementById("media_image");
